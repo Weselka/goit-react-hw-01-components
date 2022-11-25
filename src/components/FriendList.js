ae34202d1d7a6cb8e138cd8413a9c06fae8ee272
Weselka <weselyna.shvets@gmail.com>
2022-11-25 17:51:41 +0200

    create component FriendList

---
+++ src/components/FriendList.js
@@ -0,0 +1,13 @@
+import { Friend } from './Friend';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(friend => (
+        <li class="item" key={friend.id}>
+          <Friend friend={friend} />
+        </li>
+      ))}
+    </ul>
+  );
+};
