9a98671f8f9e41eb24f80cdd7e9fa560faf85207
Weselka <weselyna.shvets@gmail.com>
2022-11-25 17:51:10 +0200

    create component StatisticsList

---
+++ src/components/StatisticsList.js
@@ -0,0 +1,16 @@
+import { Statistics } from './Statistics';
+
+export const StatisticsList = ({ title, stats }) => {
+    return (
+      <section class="statistics">
+        <h2 class="title">{title}</h2>
+        <ul class="stat-list">
+          {stats.map(stat => (
+            <li class="item" key={stat.id}>
+              <Statistics stats={stat} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+};
