10bfa0ba5637bb1477608be21ca512d4ffa25841
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-11-26 10:14:03 +0200

    test style of the table

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,30 @@
 export const Transaction = ({ item: { type, amount, currency } }) => {
   return (
     <div>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
+      <td
+        style={{
+          padding: 10,
+          border: '1px solid #2a2a2a',
+        }}
+      >
+        {type}
+      </td>
+      <td
+        style={{
+          padding: 10,
+          border: '1px solid #2a2a2a',
+        }}
+      >
+        {amount}
+      </td>
+      <td
+        style={{
+          padding: 10,
+          border: '1px solid #2a2a2a',
+        }}
+      >
+        {currency}
+      </td>
     </div>
   );
 };
--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -2,13 +2,48 @@ import { Transaction } from './Transaction';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <div>
-      <table class="transaction-history">
+    <div
+      style={{
+        // display: table,
+        padding: 2,
+        borderRadius: 5,
+        border: '1px solid #999',
+      }}
+    >
+      <table
+        class="transaction-history"
+        style={{
+          // borderCollapse: collapse,
+          padding: 10,
+          border: '1px solid #2a2a2a',
+        }}
+      >
         <thead>
           <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+            <th
+              style={{
+                padding: 10,
+                border: '1px solid #2a2a2a',
+              }}
+            >
+              Type
+            </th>
+            <th
+              style={{
+                padding: 10,
+                border: '1px solid #2a2a2a',
+              }}
+            >
+              Amount
+            </th>
+            <th
+              style={{
+                padding: 10,
+                border: '1px solid #2a2a2a',
+              }}
+            >
+              Currency
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -17,7 +52,6 @@ export const TransactionHistory = ({ items }) => {
               <Transaction item={item} />
             </tr>
           ))}
-          <tr></tr>
         </tbody>
       </table>
     </div>
