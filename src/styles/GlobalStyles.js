3364d6db01354054fd52dbabbb15fb6911fd29a5
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-04 01:50:09 +0200

    changes GlobalStyles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -11,7 +11,7 @@ export const GlobalStyles = css`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
       'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
       'Helvetica Neue', sans-serif;
-    background: ${theme.colors.mainBackground};
+    background: ${theme.colors.gray};
     min-height: 100vh;
     font-style: normal;
   }
@@ -23,6 +23,7 @@ export const GlobalStyles = css`
   h6,
   p {
     margin-top: 0;
+    margin-bottom: 0;
   }
   ul,
   ol {
